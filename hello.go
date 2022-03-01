7aaf89cad3b4da58a581c8668f3c5c281c03a95b
Pei-Hsun <43227772+parsonlee2006@users.noreply.github.com>
2022-03-01 11:49:11 +0100

    Update hello.go

--- hello.go
+++ hello.go
@@ -3,7 +3,7 @@ import "fmt"
 import "rsc.io/quote"
 func main() {
     fmt.Println(quote.Glass())
-    fmt.Println("fuck you too")
-    fmt.Println("I fuck you too")
+    fmt.Println("run away too")
+    fmt.Println("I run away too")
 	
 }
